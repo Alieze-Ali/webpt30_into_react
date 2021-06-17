effa7a8de47e042c350fa8e2d71d083657a1556e
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2021-06-17 09:52:02 -0700

    added components and state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import Footer from './components/footer';
+import Header from "./components/Header"
+import Main from "./components/Main";
 
 function App() {
+
+  const [data, setData] = useState({
+    name: "Alieze",
+    location: "California"
+  })
+
+  // const dataContent = data[0]
+  // const dataSetter = data[1]
+  // console.log(dataContent, dataSetter)
+  // console.log(data)
+
   return (
     <>
-    <header>
-    App Component
-    Header
-    </header>
+    {/* is rendering the <Header></Header> from the Header.js file */ }
+    <Header />
+    {/* passing props which is an object */}
+    <Main name={ data.name } location={ data.location } />
+    <Footer />
 
-    <main>
-      Main
-    </main>
-
-    <footer>
-      Footer
-    </footer>
+    
     </>
     // you can only return one component at time
   );
